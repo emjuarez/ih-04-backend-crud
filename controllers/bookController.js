7bbcf1575dd0901951c4b50c4ac28b23befafaab
Emanueljuarez <zerauleuname@gmail.com>
2022-02-04 10:08:10 -0600

    formulario y pag individual

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -21,7 +21,47 @@ exports.getBooks = async (req, res) => {
 }
 
 exports.createBooks = async (req, res) => {
-	
+
 	return res.render("books/create")
 
+}
+
+
+exports.createBooksForm = async (req, res) => {
+
+	// 1. VERIFICAR QUE LOS DATOS DEL FORMULARIO LLEGUEN AL SERVIDOR
+	const { title, description, author, rating } = req.body
+	// const title = req.body.title
+
+
+	// 2. CREAR EL DOCUMENTO EN BASE DE DATOS
+
+	try {
+		
+		await Book.create({ title, description, author, rating })
+
+		return res.redirect("/books")	
+
+	} catch (error) {
+		
+		console.log(error)
+
+		return
+
+	}
+	
+
+
+}
+
+exports.getSingleBook = async (req, res) => {
+
+	//SABER CUAL LIBRO QUIERES LEER OBTENER EL ID DELL LIBRO
+	const { bookID } = req.params
+
+	//realizar busqueda de libro atraves de su ID
+	const getTheBook = await Book.findById(bookID)
+	res.render("books/single", {
+		book: getTheBook
+	})
 }
